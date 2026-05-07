ade0518cbf43deb5a3905c227359f543ba9100b1
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2026-05-07 19:50:30 -0300

    Adicionando registers das functions

--- src/function_app.py
+++ src/function_app.py
@@ -4,9 +4,12 @@ import azure.functions as func
 app = func.FunctionApp()
 
 # Importa triggers para registrar as functions no app
-from triggers.extract_cliente import app as extract_trigger
-from triggers.extract_entrega import app as extract_trigger
-from triggers.extract_pedido import app as extract_trigger
+from triggers.extract_cliente import app as extract_cliente
+from triggers.extract_entrega import app as extract_entrega
+from triggers.extract_pedido import app as extract_pedido
 
-app.register_functions(extract_trigger)
+# Registrar as azure functions
+app.register_functions(extract_cliente)
+app.register_functions(extract_entrega)
+app.register_functions(extract_pedido)
 logging.info("Azure Function App inicializado.")
