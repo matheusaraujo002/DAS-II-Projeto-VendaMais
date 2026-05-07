2ba3e473bd9b6ea38354d3ccac20bab6ea7f33c9
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2026-05-07 20:09:14 -0300

    Adicionando mais functions

--- src/function_app.py
+++ src/function_app.py
@@ -7,9 +7,25 @@ app = func.FunctionApp()
 from triggers.extract_cliente import app as extract_cliente
 from triggers.extract_entrega import app as extract_entrega
 from triggers.extract_pedido import app as extract_pedido
+from triggers.extract_pedido import app as extract_categoria_produto
+from triggers.extract_pedido import app as extract_estoque_movimentacao
+from triggers.extract_pedido import app as extract_estoque_saldo
+from triggers.extract_pedido import app as extract_pedido_item
+from triggers.extract_pedido import app as extract_regiao
+from triggers.extract_pedido import app as extract_representante
+from triggers.extract_pedido import app as extract_titulo_receber
+from triggers.extract_pedido import app as extract_transportadora
 
 # Registrar as azure functions
 app.register_functions(extract_cliente)
 app.register_functions(extract_entrega)
 app.register_functions(extract_pedido)
+app.register_functions(extract_categoria_produto)
+app.register_functions(extract_estoque_movimentacao)
+app.register_functions(extract_estoque_saldo)
+app.register_functions(extract_pedido_item)
+app.register_functions(extract_regiao)
+app.register_functions(extract_representante)
+app.register_functions(extract_titulo_receber)
+app.register_functions(extract_transportadora)
 logging.info("Azure Function App inicializado.")
