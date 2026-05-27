70c4e61c3b9ff6dffa76f1b88210bfbaca840957
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2026-05-27 20:19:06 -0300

    Adicionando log de tempo

--- src/triggers/extract_entrega.py
+++ src/triggers/extract_entrega.py
@@ -2,21 +2,27 @@ import azure.functions as func
 import logging
 import pyodbc
 import os
+import time
 
 app = func.Blueprint()
 
 @app.timer_trigger(schedule="0 0 6 * * *", arg_name="timer", run_on_startup=False)
 def extract_entrega(timer: func.TimerRequest) -> None:
-    
+
     sql_server = os.getenv("SQL_SERVER_SOURCE")
     database = os.getenv("SQL_DATABASE_SOURCE")
     user = os.getenv("SQL_USER_SOURCE")
     password = os.getenv("SQL_PASSWORD_SOURCE")
 
-    logging.info(f"servidor: {sql_server},  banco: {database}, usuario:{user}, senha: {password} ...")
+    logging.info(
+        f"Servidor: {sql_server}, Banco: {database}, Usuario: {user}"
+    )
 
+    query = "SELECT * FROM erp.pedido"
 
-    # Configura a string de conexão para o banco de dados SQL Server
+    tempos_execucao = []
+
+    # String de conexão ODBC
     conn_str = (
         "DRIVER={ODBC Driver 18 for SQL Server};"
         f"SERVER={sql_server};"
@@ -29,20 +35,46 @@ def extract_entrega(timer: func.TimerRequest) -> None:
     )
 
     try:
-        # Estabelece a conexão com o banco de dados usando pyodbc
-        with pyodbc.connect(conn_str) as conn:
-            # Cria um cursor para executar a consulta   
-            cursor = conn.cursor()
-            
-            query = "select top 5 * from erp.pedido"
 
-            # Executa a consulta SQL
-            cursor.execute(query)
+        # Executa 2 vezes
+        for i in range(2):
 
-            # Busca todos os resultados da consulta
-            rows = cursor.fetchall()
+            logging.info(f"Iniciando teste {i + 1} com pyodbc")
 
-            logging.info(rows)           
+            # Marca início
+            inicio = time.perf_counter()
+
+            # Abre conexão
+            with pyodbc.connect(conn_str) as conn:
+
+                cursor = conn.cursor()
+
+                # Executa SELECT
+                cursor.execute(query)
+
+                # Carrega TODAS as linhas em memória
+                rows = cursor.fetchall()
+
+            # Marca fim
+            fim = time.perf_counter()
+
+            # Calcula duração
+            duracao = fim - inicio
+
+            tempos_execucao.append(duracao)
+
+            logging.info(
+                f"Teste {i + 1} concluído - "
+                f"{len(rows)} linhas carregadas em "
+                f"{duracao:.4f} segundos"
+            )
+
+        # Média
+        tempo_medio = sum(tempos_execucao) / len(tempos_execucao)
+
+        logging.info(
+            f"Tempo médio de SELECT: {tempo_medio:.4f} segundos"
+        )
 
     except Exception as e:
         logging.error(f"Erro ao ler erp.pedido: {str(e)}")
